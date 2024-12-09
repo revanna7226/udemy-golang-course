4b9daf624068da2fa22246f7510bd9adeac1da99
pure-wedding <pure.couple.2024@gmail.com>
2024-12-09 12:54:48 +0530

    Added deck newCards()

---
+++ projects/cards/deck.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+// create a new custom type deck
+// which is of type slice of strings
+// means a kind of deck extends []string
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Club"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
